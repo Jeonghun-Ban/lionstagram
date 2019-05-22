5b99e9e841baa4e28ac87d174574cb684067e5d7
Jeonghun <bjh5230@gmail.com>
2019-05-22 23:06:13 +0900

    Add Post model field : image

--- lionstagram/urls.py
+++ lionstagram/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import page.views
+# media 사용시 필요
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', page.views.home, name='home'),
     path('detail/<int:post_id>', page.views.detail, name='detail'),
-]
+] + static('/media/', document_root=settings.MEDIA_ROOT)
--- page/models.py
+++ page/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     # id 필드를 선언하지 않을 경우 자동적으로 객체 생성시마다 1부터 순차 부여됨
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     pub_date = models.DateTimeField('publish')
+    image = models.ImageField(upload_to='images/', blank=True)
     content = models.TextField()
 
     def __str__(self):
